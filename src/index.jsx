a66d2794a852270d5c48f7085573c678cdf9d8fd
eduardovarela27 <eduardoemanuelvarela@gmail.com>
2023-03-14 20:56:45 -0600

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Container } from './components/Container';
+import  './index.css';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Container/>
+    </React.StrictMode>
+)
